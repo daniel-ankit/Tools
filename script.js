d2dacc8e24a8d46cfc92438db657378e91439c63
Ankit Kumar <ankit.kumar.061000@gmail.com>
2022-01-19 23:21:34 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const click = new Audio();
 click.src = "../click.mp3";
 
 const body = document.querySelector('body');
+const a = document.querySelectorAll('a');
 
 toggle.addEventListener("click", ()=>{
     toggle.classList.toggle('active');
     body.classList.toggle('active');
+    for(var i=0; i<15; i++){
+        a[i].classList.toggle('active');
+    }
     click.play();
 })
