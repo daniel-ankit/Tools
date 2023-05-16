ae16c12d5a936fb35abc6c56c780f3ea4075395c
Ankit Kumar <ankit.kumar.061000@gmail.com>
2023-05-16 12:42:44 +0530

    Updates

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const body = document.querySelector('body');
 const a = document.querySelectorAll('a');
 
 toggle.addEventListener("click", ()=>{
+    click.play();
     toggle.classList.toggle('active');
     body.classList.toggle('active');
     for(var i=0; i<11; i++){
         a[i].classList.toggle('active');
     }
-    click.play();
 })
